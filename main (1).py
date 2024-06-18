e31e9e38a86db043778bd58343192fd0fbf3b124
Hiral Vora <168340234+hiral2004@users.noreply.github.com>
2024-06-19 00:10:46 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,7 @@
-def fibonacci(n):
-  sequence=[0, 1]
-  while len(sequence) < n:
-    next_value= sequence[-1] + sequence[-2]
-    sequence.append(next_value)
-  return sequence
+def print_positive_numbers(numbers):
+  for num in numbers:
+    if num>0:
+      print(num)
 
-num_terms=20
-fib_sequence=fibonacci(num_terms)
-print('Fibonacci sequence with {num_terms} terms is: {fib_sequence}'  .format(num_terms=num_terms, fib_sequence=fib_sequence))
+numbers=[12, -7, 5, 64, -14]
+print_positive_numbers(numbers)
